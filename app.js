5850ef2af1a8ec17a236fce4ddf41f1ed2fe0602
macisongithub <44820315+macisongithub@users.noreply.github.com>
2018-11-07 18:49:24 +0000

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const fs = require('fs');
 const db = require('quick.db');
-const yt = require('ytdl-core');
 
 const config = require("./config.json");
 
