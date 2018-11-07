b9b7927cec33760723b8fc40b689a3edaee9f610
macisongithub <44820315+macisongithub@users.noreply.github.com>
2018-11-07 20:15:33 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const fs = require("fs");
 
 client.on("ready", () => {
   console.log(`Launched. Defined as BOT. Username is ${client.user.username}.`);
