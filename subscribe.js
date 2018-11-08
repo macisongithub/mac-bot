e2289a4b5a8fe4865519482bc17e4a1ea3817137
macisongithub <44820315+macisongithub@users.noreply.github.com>
2018-11-08 18:29:24 +0000

    Create subscribe.js

---
+++ subscribe.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+const fs = require('fs');
+
+exports.run = (bot, message, args, func) => {
+
+	message.channel.send(message.author.toString() + ", You are now subscribed to announcements on the Midnight discord server.")
+
+	messafe.author.addRole("<@&387627056235544576>");
+}
