033d4ea96da2295d3e42e7034c7bdfd84daa5dac
macisongithub <44820315+macisongithub@users.noreply.github.com>
2018-11-08 19:14:20 +0000

    Update setwelcome.js

--- setwelcome.js
+++ setwelcome.js
@@ -1,6 +1,6 @@
 const db = require('quick.db')
 
-exports.run = (bot, message, args, func) => {
+exports.run = (client, message, args, func) => {
 
     if (!message.member.roles.find('name', 'Owner')) return func.embed(message.channel, '**This command requires you to have the Owner role.**', 120000)
     if (!args.join(" ") && args.join(" ").toLowerCase() !== 'none') return func.embed(message.channel, '**Please provide a message.**\n > *>>setwelcome <message>*')
