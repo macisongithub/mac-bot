b7f325ee06c9f0150eedf3aa99fb5eec606dd9d2
macisongithub <44820315+macisongithub@users.noreply.github.com>
2018-11-08 19:43:21 +0000

    Delete edit.js

--- edit.js
+++
@@ -1,6 +0,0 @@
-const Discord = require('discord.js');
-
-exports.run = (client, message, args, func) => {
-
-	message.channel.send("Hello.").then(sentMessage => sentMessage.edit("Hello.")).then(sentMessage => sentMessage.edit("Hello.")).then(sentMessage => sentMessage.edit("Hello.")).then(sentMessage => sentMessage.edit("I am a bot created and maintained by <@295978095129657355>.")).then(sentMessage => sentMessage.edit("I am a bot created and maintained by <@295978095129657355>.")).then(sentMessage => sentMessage.edit("I am a bot created and maintained by <@295978095129657355>.")).then(sentMessage => sentMessage.edit("Thank you for reading this."));
-}
