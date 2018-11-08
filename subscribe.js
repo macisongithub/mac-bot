af6563bceffaf740421410bad6ed14c51096d9fa
macisongithub <44820315+macisongithub@users.noreply.github.com>
2018-11-08 19:13:13 +0000

    Update subscribe.js

--- subscribe.js
+++ subscribe.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const fs = require('fs');
 
-exports.run = (bot, message, args, func) => {
+exports.run = (client, message, args, func) => {
 
 	message.channel.send(message.author.toString() + ", You are now subscribed to announcements on the Midnight discord server.")
 
