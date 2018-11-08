1c25df8962f21b19345f355076c98f4b158cee0b
macisongithub <44820315+macisongithub@users.noreply.github.com>
2018-11-08 19:16:54 +0000

    Update invite.js

--- invite.js
+++ invite.js
@@ -1,11 +1,11 @@
 const Discord = require('discord.js');
 
-exports.run = (bot, message, args, func) => {
+exports.run = (client, message, args, func) => {
 
             const embed = new Discord.RichEmbed()
                 .setAuthor("Join our discord!")
                 .setColor(0x9F45FF)
-                .setDescription("https://discord.io/midnight-development")
+                .setDescription("https://discord.gg/6PFN5f9")
                 .setThumbnail("https://cdn.discordapp.com/attachments/376764803596943383/414762325229305866/JPEG_20180211_191145.jpg")
 
                 message.channel.send({embed});
