8441618205922e626b5434a279ba8e66d814d4d1
macisongithub <44820315+macisongithub@users.noreply.github.com>
2018-11-08 18:49:40 +0000

    Update announce.js

--- announce.js
+++ announce.js
@@ -9,7 +9,7 @@ exports.run = (bot, message, args, func) => {
 		.setColor(0x9F45FF)
 		.setDescription(args.join(" "))
 
-		bot.guilds.get("479268202866540564").channels.get("489498856166522882").send("<@&489504532200554513>");
-        bot.guilds.get("479268202866540564").channels.get("489498856166522882").send({embed});
+		client.guilds.get("479268202866540564").channels.get("489498856166522882").send("<@&489504532200554513>");
+        client.guilds.get("479268202866540564").channels.get("489498856166522882").send({embed});
 
 }
