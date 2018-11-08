00ff3060e067d1745a4818fe98728de8fea10673
macisongithub <44820315+macisongithub@users.noreply.github.com>
2018-11-08 18:52:32 +0000

    Update subscribe.js

--- subscribe.js
+++ subscribe.js
@@ -5,5 +5,5 @@ exports.run = (bot, message, args, func) => {
 
 	message.channel.send(message.author.toString() + ", You are now subscribed to announcements on the Midnight discord server.")
 
-	messafe.author.addRole("<@&387627056235544576>");
+	message.author.addRole("<@&387627056235544576>");
 }
