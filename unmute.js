35140553bd8ad58f875d0738d9e1a8ad8656c818
macisongithub <44820315+macisongithub@users.noreply.github.com>
2018-11-08 18:29:41 +0000

    Create unmute.js

---
+++ unmute.js
@@ -0,0 +1,22 @@
+const fs = require("fs")
+
+exports.run = async (bot, message, args) => {
+if(!message.member.hasPermission("MANAGE_ROLES")) return message.channel.send("<:no:408996763748270080> You must have the MANAGE_ROLES permission in this discord server to use this command!");
+
+		let toMute = message.guild.member(message.mentions.users.first()) || message.guild.member(args[0]);
+		if(!toMute) return message.channel.send("<:no:408996763748270080> You did not mention a user or specify their ID!");
+
+		let role = message.guild.roles.find(r => r.name === "Muted");	
+		
+		if(!role || !toMute.roles.has(role.id)) return message.channel.send(`:radioactive: **${toMute.user.tag}** is not muted!`);
+
+		await toMute.removeRole(role);
+
+			delete bot.mutes[toMute.id];
+
+			fs.writeFile("./mutes.json", JSON.stringify(bot.mutes), err => {
+				if(err) throw err;
+				console.log(`Successfully unmuted ${toMute.user.tag}.`);
+			});
+		message.channel.send(`<:yes:408996738217541632> Successfully unmuted **${toMute.user.tag}**.`);
+}
