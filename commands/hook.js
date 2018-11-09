052817d98d53b309f9f358bcc38e2cb5f5dc6f09
macisongithub <44820315+macisongithub@users.noreply.github.com>
2018-11-09 16:27:08 +0000

    Create hook.js

---
+++ commands/hook.js
@@ -0,0 +1,12 @@
+exports.run = (client, message, args, func) => {
+
+        message.delete();
+
+        if (msg === prefix + 'hook') {
+            return func.hook(message.channel, 'Hook Usage', `${prefix}hook <title>, <message>, [HEXcolor], [avatarURL]\n\n**<> is required\n[] is optional**`,'9F45FF','https://cdn4.iconfinder.com/data/icons/global-logistics-3/512/129-512.png')
+        }
+
+        let hookArgs = message.content.slice(prefix.length + 4).split(",");
+        
+        func.hook(message.channel, hookArgs[0], hookArgs[1], hookArgs[2], hookArgs[3]);
+}
