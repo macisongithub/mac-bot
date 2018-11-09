485f985446e5c5e53ed80f54f2763ab9d5c4f793
macisongithub <44820315+macisongithub@users.noreply.github.com>
2018-11-09 16:45:47 +0000

    Create userinfo.js

---
+++ commands/userinfo.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js')
+
+exports.run = (client, message, args, func) => {
+
+	const embed = new Discord.RichEmbed()
+		.setAuthor(message.author.username)
+		.setDescription("Information about your discord account.")
+		.setColor(0x9F45FF)
+		.addField("Full Username", `${message.author.username}#${message.author.discriminator}`)
+		.addField("ID", message.author.id)
+		.addField("Created At", message.author.createdAt)
+
+	message.channel.send({embed});
+}
